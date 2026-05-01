fc5ce323cad7fc6dd76906512b493774410ca1be
xat99 <lornadoon10@hotmail.com>
2026-05-01 17:12:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,25 +4,33 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
-# Importáljuk a start_miner függvényt a run.py-ból
+# Ez a sor "húzza be" a bányász kódot a másik fájlból
 from run import start_miner 
 
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key')
+# Biztonsági kulcs a munkamenetekhez
+app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key_123')
 
 # MongoDB csatlakozás beállítása
+# Render-en a MONGO_URI környezeti változóba a MongoDB Atlas linkedet másold!
 mongo_url = os.getenv('MONGO_URI', 'mongodb://mongodb:27017/')
 client = MongoClient(mongo_url)
 db = client['twitch_miner_web']
 users_collection = db['users']
 twitch_data_collection = db['twitch_data']
 
-# Admin beállítása (szaby / 2003)
+# Admin adatok (szaby / 2003)
 ADMIN_USER = os.getenv('WEB_USERNAME', 'szaby')
 ADMIN_PASS = os.getenv('WEB_PASSWORD', '2003')
-users_collection.update_one({'username': ADMIN_USER}, {'$set': {'password': ADMIN_PASS}}, upsert=True)
+
+# Admin felhasználó frissítése/létrehozása az adatbázisban
+users_collection.update_one(
+    {'username': ADMIN_USER}, 
+    {'$set': {'password': ADMIN_PASS}}, 
+    upsert=True
+)
 
 @app.route('/')
 def home():
@@ -51,11 +59,13 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    # 1. Elindítjuk a bányászt a háttérben (külön szálon)
-    # Így a bot fut, de a program megy tovább a weboldalhoz
+    # --- AUTOMATIKUS INDÍTÁS ---
+    # Elindítjuk a bányászt egy háttérszálon, így nem blokkolja a weboldalt
+    print("Log: A bányász indítása a háttérben...")
     miner_thread = threading.Thread(target=start_miner, daemon=True)
     miner_thread.start()
 
-    # 2. Elindítjuk a Flask weboldalt (ez nyitja meg a portot a Rendernek)
+    # Elindítjuk a Flask-et a Render által kiosztott porton
     port = int(os.environ.get("PORT", 5000))
+    print(f"Log: A weboldal elindul a {port}-os porton...")
     app.run(host='0.0.0.0', port=port)
